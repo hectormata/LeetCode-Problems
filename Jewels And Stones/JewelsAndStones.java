f22f4634fef79ee07c0b0924f59ac6c37d44c7af
hectormata <hector.mata9@gmail.com>
2019-02-22 15:54:40 -0500

    Create JewelsAndStones.java

---
+++ Jewels And Stones/JewelsAndStones.java
@@ -0,0 +1,52 @@
+/*
+ * You're given strings J representing the types of stones that are jewels, 
+ * and S representing the stones you have.  Each character in S is a type of 
+ * stone you have. You want to know how many of the stones you have are also 
+ * jewels. The letters in J are guaranteed distinct, and all characters in J 
+ * and S are letters. Letters are case sensitive, so "a" is considered a 
+ * different type of stone from "A".
+ * 
+ * Example 1:
+ * 
+ * Input: J = "aA", S = "aAAbbbb"
+ * Output: 3
+ * 
+ * Example 2:
+ * 
+ * Input: J = "z", S = "ZZ"
+ * Output: 0
+ * Note:
+ * 
+ * S and J will consist of letters and have length at most 50.
+ * The characters in J are distinct.
+ * 
+ */
+public class JewelsAndStones {
+
+	public static int numJewelsInStones(String j, String s) {
+		
+		int output = 0;
+		int sizeOfJ = j.length();
+		int sizeOfS = s.length();
+		
+		for (int i = 0; i < sizeOfJ; i++) {
+			for (int k = 0; k < sizeOfS; k++) {
+				
+				if (j.charAt(i) == s.charAt(k)) {
+					output = output + 1;
+				}
+			}
+		}
+		return output;
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String strJ = "aA";
+		String strS = "aAAbbbb";
+		
+		System.out.println(numJewelsInStones(strJ, strS));
+	}
+
+}
