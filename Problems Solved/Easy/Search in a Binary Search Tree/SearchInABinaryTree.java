fa549408adef92c4ce70e1f3ed54611721bfefa6
hectormata <hector.mata9@gmail.com>
2019-05-06 15:35:48 -0400

    Create SearchInABinaryTree.java

---
+++ Problems Solved/Easy/Search in a Binary Search Tree/SearchInABinaryTree.java
@@ -0,0 +1,60 @@
+/*
+ * Given the root node of a binary search tree (BST) and a value. You need to 
+ * find the node in the BST that the node's value equals the given value. 
+ * Return the subtree rooted with that node. If such node doesn't exist, you 
+ * should return NULL.
+ * 
+ * For example, 
+ * 
+ * Given the tree:
+ *         4
+ *        / \
+ *       2   7
+ *      / \
+ *     1   3
+ *     
+ * And the value to search: 2
+ * You should return this subtree:
+ * 
+ * 	  2     
+ *   / \   
+ *  1   3
+ *  
+ * In the example above, if we want to search the value 5, since there is no 
+ * node with value 5, we should return NULL.
+ * 
+ * Note that an empty tree is represented by NULL, therefore you would see the
+ * expected output (serialized tree format) as [], not null.
+ * 
+ */
+public class SearchInABinaryTree {
+
+    public static TreeNode searchBST(TreeNode root, int val) {
+		
+    	if (root == null) {
+    		return root;
+    	}
+    	
+    	if(root.val == val) {
+    		return root;
+    	}
+    	else {
+    		return val<root.val ? searchBST(root.left, val): searchBST(root.right, val);
+    	}     
+    }
+    
+    public static void main(String[] args) {
+		
+    	TreeNode root = new TreeNode(1);
+    	
+    	root.left = new TreeNode(1);
+    	root.right = new TreeNode(2);
+    	root.left = new TreeNode(3);
+    	root.right = new TreeNode(4);
+    	root.left = new TreeNode(7);
+    	
+    	System.out.println(searchBST(root, 2));
+    	
+    	
+	}
+}
